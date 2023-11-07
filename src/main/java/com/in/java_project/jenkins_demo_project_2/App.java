8c5dd0fb2b6a2e997e695745900af6db231019c9
mrkhan9989 <mazharkhan687@gmail.com>
2023-11-07 07:58:11 +0530

    project added

---
+++ src/main/java/com/in/java_project/jenkins_demo_project_2/App.java
@@ -0,0 +1,14 @@
+package com.in.java_project.jenkins_demo_project_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Simple Project" );
+        System.out.println( "this is created for demo purpose" );
+    }
+}
---
+++ src/test/java/com/in/java_project/jenkins_demo_project_2/AppTest.java
@@ -0,0 +1,20 @@
+package com.in.java_project.jenkins_demo_project_2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
